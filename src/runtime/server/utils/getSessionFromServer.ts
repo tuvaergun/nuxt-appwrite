7cc815bc5845f9454d5a778d1e30c2e1d4853ee7
Yasin Ergün <yasin@ergn.org>
2023-03-03 15:02:19 +0300

    Update getSessionFromServer.ts

--- src/runtime/server/utils/getSessionFromServer.ts
+++ src/runtime/server/utils/getSessionFromServer.ts
@@ -10,22 +10,23 @@ client
 const account = new Account(client);
 
 export const getSessionFromServer = async (cookies: Record<string, string>): Promise<boolean | undefined> => {
-  const sessionCookiesNames = [
-    `a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}`,
-    `a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}_legacy`,
-  ];
+  try {
+    const sessionCookiesNames = [
+      `a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}`,
+      `a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}_legacy`,
+    ];
 
-  let hash = cookies[sessionCookiesNames[0]] ?? cookies[sessionCookiesNames[1]] ?? '';
+    let hash = cookies[sessionCookiesNames[0]] ?? cookies[sessionCookiesNames[1]] ?? '';
 
-  const authCookies: any = new Map();
-  authCookies[`a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}`] = hash;
+    const authCookies: any = new Map();
+    authCookies[`a_session_${config.public.appwrite.APPWRITE_PROJECT_ID.toLowerCase()}`] = hash;
 
-  client.headers['X-Fallback-Cookies'] = JSON.stringify(authCookies);
+    client.headers['X-Fallback-Cookies'] = JSON.stringify(authCookies);
 
-  const user = await account.get()
-  if (user.$id) {
+    await account.get()
+    
     return true
-  } else {
+  } catch (e) {
     return undefined
   }
 }
